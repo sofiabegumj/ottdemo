28e814a94040a3ed996b239fa410802d2ab615b8
sofiabegum.j <sofiabegum.j@tataelxsi.co.in>
2020-06-03 12:52:24 +0530

    application name change in pom

--- src/main/java/com/ott/demo/models/Customer.java
+++ src/main/java/com/ott/demo/models/Customer.java
@@ -8,7 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Customer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
